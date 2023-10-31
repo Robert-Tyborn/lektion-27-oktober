0001752c57ff505b32b28d2e5e53b92e9b1351d1
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-10-31 09:15:03 +0100

    Added code

---
+++ card-game/js/index.js
@@ -0,0 +1,6 @@
+/**
+Enkelt kortspel där spelaren gissar om nästa kort som dras ur högen
+är högre, lägre eller likvärdigt. Poäng ges vid rätt gissning. 
+Spelet är slut när kortleken är slut, eller man gissat fel 3 ggr.
+*/
+
